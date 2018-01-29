cd3f74b9f8bad2b83221fce995e99d3dac8bf7db
William Oldham <wegg7250@gmail.com>
2018-01-29 08:05:43 +0000

    More stuff

--- js/main.js
+++ js/main.js
@@ -1,24 +1,27 @@
 var commands = {
-    "clear": {
-        "help": "Clears the terminal",
-        "callback": function(terminal) {
+    clear: {
+        help: "Clears the terminal",
+        callback: function(terminal) {
             terminal.clear();
-        }
+        },
+        display: true
     },
-    "help": {
-        "help": "",
-        "callback": helpCommand
-    }
-    ,
-    "helpppppppppp": {
-        "help": "",
-        "callback": helpCommand
+    help: {
+        help: "",
+        callback: helpCommand
+    },
+    flarebot: {
+        help: "Super cool thing ;)",
+        callback: function(terminal) {
+            terminal.echo("FlareBot is some super cool shit, check it out here: https://flarebot.stream");
+        },
+        display: true
     }
 };
 
 
 jQuery(function($, undefined) {
-    $('body').terminal(function(command) {
+    $('#terminal').terminal(function(command) {
         if (command !== '') {
             if (commands[command] !== undefined) {
                 commands[command].callback(this);
@@ -28,9 +31,19 @@ jQuery(function($, undefined) {
         }
     }, {
         completion: Object.keys(commands),
-        greetings: false,
+        greetings: " ____  _                         ____                 _                 _ \n" +
+                   "|  _ \\(_)                       / __ \\               | |               | |\n" + 
+                   "| |_) |_ _ __   __ _ _ __ _   _| |  | __   _____ _ __| | ___   __ _  __| |\n" +
+        "|  _ <| | '_ \\ / _` | '__| | | | |  | \\ \\ / / _ | '__| |/ _ \\ / _` |/ _` |\n" + 
+        "| |_) | | | | | (_| | |  | |_| | |__| |\\ V |  __| |  | | (_) | (_| | (_| |\n" +
+        "|____/|_|_| |_|\\__,_|_|   \\__, |\\____/  \\_/ \\___|_|  |_|\\___/ \\__,_|\\__,_|\n" +
+        "                           __/ |                                          \n" +
+        "                          |___/                                           \n" +
+        "[[b;;]Full-time student, programmer and tech enthusiast. Currently working on FlareBot]\n\n" + 
+        "Type [[b;lightblue;]whoami] to get information about me or [[b;lightblue;]help] to see all the commands you can do!",
         name: 'binaryoverload',
-        height: 200,
+        height: "100%",
+        width: "100%",
         prompt: '> ',
         clear: false,
         exit: false    
@@ -39,8 +52,8 @@ jQuery(function($, undefined) {
 
 function helpCommand(terminal) {
     var commandsArray = Object.keys(commands);
-    remove(commandsArray, "help");
     for (var i = 0; i < commandsArray.length; i++) {
+        if (!commands[commandsArray[i]].display) continue;
         terminal.echo(formatCommand(maxCommandLength(), commandsArray[i], commands[commandsArray[i]].help));
     }
 }
