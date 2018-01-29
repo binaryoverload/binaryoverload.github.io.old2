dbc6f476258a3a2a66cfe5c3b551e266269f997e
William Oldham <wegg7250@gmail.com>
2018-01-29 20:21:16 +0000

    Add better way to handle long commands

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ var commands = {
         help: "Clears the terminal",
         callback: function(terminal) {
             terminal.clear();
+            terminal.greetings();
         },
         display: true
     },
@@ -12,9 +13,12 @@ var commands = {
     },
     flarebot: {
         help: "Super cool thing ;)",
-        callback: function(terminal) {
-            terminal.echo("FlareBot is some super cool shit, check it out here: https://flarebot.stream");
-        },
+        callback: echoFile,
+        display: true
+    },
+    whoami: {
+        help: "Displays contact information about me!",
+        callback: echoFile,
         display: true
     }
 };
@@ -23,30 +27,33 @@ var commands = {
 jQuery(function($, undefined) {
     $('#terminal').terminal(function(command) {
         if (command !== '') {
+            command = command.trim();
             if (commands[command] !== undefined) {
-                commands[command].callback(this);
+                commands[command].callback(this, command);
+            } else {
+                this.echo("[[b;red;]That is not a command!]")
             }
         } else {
-           this.echo('');
+            this.echo('');
         }
     }, {
         completion: Object.keys(commands),
         greetings: " ____  _                         ____                 _                 _ \n" +
-                   "|  _ \\(_)                       / __ \\               | |               | |\n" + 
-                   "| |_) |_ _ __   __ _ _ __ _   _| |  | __   _____ _ __| | ___   __ _  __| |\n" +
-        "|  _ <| | '_ \\ / _` | '__| | | | |  | \\ \\ / / _ | '__| |/ _ \\ / _` |/ _` |\n" + 
-        "| |_) | | | | | (_| | |  | |_| | |__| |\\ V |  __| |  | | (_) | (_| | (_| |\n" +
-        "|____/|_|_| |_|\\__,_|_|   \\__, |\\____/  \\_/ \\___|_|  |_|\\___/ \\__,_|\\__,_|\n" +
-        "                           __/ |                                          \n" +
-        "                          |___/                                           \n" +
-        "[[b;;]Full-time student, programmer and tech enthusiast. Currently working on FlareBot]\n\n" + 
-        "Type [[b;lightblue;]whoami] to get information about me or [[b;lightblue;]help] to see all the commands you can do!",
+            "|  _ \\(_)                       / __ \\               | |               | |\n" +
+            "| |_) |_ _ __   __ _ _ __ _   _| |  | __   _____ _ __| | ___   __ _  __| |\n" +
+            "|  _ <| | '_ \\ / _` | '__| | | | |  | \\ \\ / / _ | '__| |/ _ \\ / _` |/ _` |\n" +
+            "| |_) | | | | | (_| | |  | |_| | |__| |\\ V |  __| |  | | (_) | (_| | (_| |\n" +
+            "|____/|_|_| |_|\\__,_|_|   \\__, |\\____/  \\_/ \\___|_|  |_|\\___/ \\__,_|\\__,_|\n" +
+            "                           __/ |                                          \n" +
+            "                          |___/                                           \n" +
+            "[[b;;]Full-time student, programmer and tech enthusiast. Currently working on FlareBot]\n\n" +
+            "Type [[b;lightblue;]whoami] to get information about me or [[b;lightblue;]help] to see all the commands you can do!",
         name: 'binaryoverload',
         height: "100%",
         width: "100%",
         prompt: '> ',
         clear: false,
-        exit: false    
+        exit: false
     });
 });
 
@@ -58,9 +65,32 @@ function helpCommand(terminal) {
     }
 }
 
+function whoamiCommand(terminal) {
+    var string = "[[b;#17BEBB;]Hi there!]";
+    terminal.echo(string);
+}
+
+function echoFile(terminal, command) {
+    var xhr;
+
+    xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = handleStateChange;
+    xhr.open("GET", "commands/" + command + ".ansi", true);
+    xhr.send();
+
+    function handleStateChange() {
+        if (xhr.readyState === 4) {
+            if (xhr.status == 200) {
+                console.log(xhr);
+                terminal.echo(xhr.responseText);
+            }
+        }
+    }
+}
+
 function remove(array, element) {
     var index = array.indexOf(element);
-    
+
     if (index !== -1) {
         array.splice(index, 1);
     }
@@ -76,5 +106,5 @@ function maxCommandLength() {
 }
 
 function formatCommand(maxLength, command, help) {
-    return "[[;green;]" + command + "]" + " ".repeat((maxLength - command.length) + 5) + help;
+    return "[[;#6BBAEC;]" + command + "]" + " ".repeat((maxLength - command.length) + 5) + help;
 }
